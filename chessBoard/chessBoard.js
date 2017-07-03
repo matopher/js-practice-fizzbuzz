736ffa97ff651ad22a82214ab9df3f008b61870d
Matt Woods <mattwoods21@gmail.com>
2017-07-02 23:26:22 -0500

    added better solution

--- chessBoard/chessBoard.js
+++ chessBoard/chessBoard.js
@@ -1,9 +1,15 @@
-function makeBoard(size) {
-	for (i = 1; i <= size; i++) {
-		console.log(" # # # #");
-		// console.log("\n")
-		console.log("# # # #");
-	}	
+var size = 8;
+
+var board ="";
+
+for (var y = 0; y < size; y++) {
+	for (var x = 0; x < size; x++) {
+		if ((x + y) % 2 == 0)
+			board += " ";
+		else
+			board += "#";
+	}
+	board += "\n";
 }
 
-console.log(makeBoard(4));
+console.log(board);
